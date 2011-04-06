8f0c9a61cfe069855f1caa51474d5ee959fe6f31
Morgan Goose <morgan.goose@gmail.com>
2011-04-06 10:47:23 -0400

    not working, but I'll have to come back to it as I'm going to have to make it execute it in parallel

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -17,21 +17,41 @@ from fabric.io import output_loop
 import fabric.network # So I can call patch_object correctly. Sigh.
 from fabric.state import env, output, _get_system_username
 from fabric.operations import run, sudo
-from fabric.decorators import runs_parallel
+from fabric.decorators import runs_parallel, hosts
 
 from utils import *
 from server import (server, PORT, RESPONSES, PASSWORDS, CLIENT_PRIVKEY, USER,
     CLIENT_PRIVKEY_PASSPHRASE)
 
-class TestParallel(FabricTest):
 
-    @server()
-    @runs_parallel
-    def test_runs_parallel(self):
-        """
-        Want to do a simple call and respond
-        """
-        env.pool_size = 10
-        cmd = "ls /simple"
-        with hide('everything'):
+@server(port=2220)
+@server(port=2221)
+@server(port=2222)
+@server(port=2223)
+@server(port=2224)
+@server(port=2225)
+@server(port=2226)
+@server(port=2227)
+@server(port=2228)
+@server(port=2229)
+@server(port=2230)
+
+
+
+def test_runs_parallel():
+    """
+    Do a simple call and respond, in parallel
+    """
+
+    @runs_parallel(5)
+    @hosts(['127.0.0.1:%s' % x for x in range(2220,2231)])
+    def foo():
+#   ports = range(2220,2231)
+#   hosts = map(lambda x: '127.0.0.1:%s' % x, ports)
+#   with settings(all_hosts=hosts, run_in_parallel=True, pool_size=5):
+#       for port in ports:
+        with settings(hide('everything')):
+            cmd = "ls /simple"
+            print run(cmd), env.host_string
             eq_(run(cmd), RESPONSES[cmd])
+
